29c2a747158a8955c694dc6c7acfefed21a365b1
Ed Muhlestein <emuhlestein@yahoo.com>
2016-03-17 16:40:48 -0500

    made recommended changes: Used style for buttons. Defined colors in color.xml Added ScrollView Added onClicks

--- app/src/main/java/com/intelliviz/myappportfolio/MainActivity.java
+++ app/src/main/java/com/intelliviz/myappportfolio/MainActivity.java
@@ -3,68 +3,37 @@ package com.intelliviz.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mSpotifyButton;
-    private Button mScoresAppButton;
-    private Button mLibAppButton;
-    private Button mBuildItBiggerButton;
-    private Button mXYZReaderButton;
-    private Button mMyAppButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        mSpotifyButton = (Button)findViewById(R.id.spotify_streamer_id);
-        mSpotifyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch the Spotify Streamer app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onSpotifyStreamerClick(View view) {
+        Toast.makeText(this, "This button will launch the Spotify Streamer app", Toast.LENGTH_LONG).show();
+    }
 
-        mScoresAppButton = (Button)findViewById(R.id.scores_app_id);
-        mScoresAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch the Scores app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onScoresAppClick(View view) {
+        Toast.makeText(this, "This button will launch the Scores app", Toast.LENGTH_LONG).show();
+    }
 
-        mLibAppButton = (Button)findViewById(R.id.lib_app_id);
-        mLibAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch the Library app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onLibraryAppClick(View view) {
+        Toast.makeText(this, "This button will launch the Library app", Toast.LENGTH_LONG).show();
+    }
 
-        mBuildItBiggerButton = (Button)findViewById(R.id.build_it_bigger_id);
-        mBuildItBiggerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch the Build It Bigger app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onBuildItBiggerClick(View view) {
+        Toast.makeText(this, "This button will launch the Build It Bigger app", Toast.LENGTH_LONG).show();
+    }
 
-        mXYZReaderButton = (Button)findViewById(R.id.xyz_reader_id);
-        mXYZReaderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch the XYZ Reader app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onXYZReaderClick(View view) {
+        Toast.makeText(this, "This button will launch the XYZ Reader app", Toast.LENGTH_LONG).show();
+    }
 
-        mMyAppButton = (Button)findViewById(R.id.my_app_id);
-        mMyAppButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button will launch my capstone app", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onCapStoneAppClick(View view) {
+        Toast.makeText(this, "This button will launch my capstone app", Toast.LENGTH_LONG).show();
     }
 }
